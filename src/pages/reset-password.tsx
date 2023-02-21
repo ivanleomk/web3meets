71ea98efc71719a1f9c5417c9d68222a46a343f1
ivanleo <ivanleomk@gmail.com>
2023-02-21 21:30:19 +0800

    Fixed up a password reset functionality

---
+++ src/pages/reset-password.tsx
@@ -0,0 +1,76 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/router";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { ClipLoader } from "react-spinners";
+import { toast } from "react-toastify";
+import { Button } from "../components/Button";
+import Input from "../components/Input";
+import SectionHeader from "../components/SectionHeader";
+import { userResetEmailType, userResetEmailSchema } from "../types/auth";
+import { Database } from "../types/database-raw";
+
+const ResetPassword = () => {
+  const supabaseClient = useSupabaseClient<Database>();
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm<userResetEmailType>({
+    resolver: zodResolver(userResetEmailSchema),
+  });
+
+  const router = useRouter();
+  const onSubmit = async (credentials: userResetEmailType) => {
+    console.log("---Triggering on Submit");
+    await supabaseClient.auth
+      .signInWithOtp({
+        email: credentials.email,
+      })
+      .then((res) => {
+        toast.success("Email Link succesfully sent");
+        void router.push("/email-link");
+      })
+      .catch(() => {
+        toast.success("Email Link succesfully sent");
+      });
+  };
+
+  return (
+    <div className="mx-auto max-w-3xl">
+      <SectionHeader
+        title="Forgot your password?"
+        subtitle="Enter your email below for a one-time signup link"
+      >
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="my-4" />
+          <Input
+            label="Email"
+            errorMessage={errors?.email}
+            htmlFor="email"
+            autocomplete="email"
+            type="text"
+            {...register("email")}
+          />
+          <div className="mt-10 flex items-center justify-center">
+            <button
+              className="inline-flex justify-center rounded-lg bg-gray-800 py-2 px-3 text-sm font-semibold text-white outline-2 outline-offset-2 transition-colors hover:bg-gray-900 active:bg-gray-800 active:text-white/80"
+              type="submit"
+            >
+              {isSubmitting ? (
+                <>
+                  <ClipLoader color="white" size={20} />
+                </>
+              ) : (
+                <>Get Magic Link</>
+              )}
+            </button>
+          </div>
+        </form>
+      </SectionHeader>
+    </div>
+  );
+};
+
+export default ResetPassword;
--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,7 +1,59 @@
-import React from "react";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import { Button } from "../components/Button";
+import SectionHeader from "../components/SectionHeader";
 
 const Settings = () => {
-  return <div>Settings</div>;
+  const supabaseClient = useSupabaseClient();
+  const [password, setPassword] = useState("");
+  return (
+    <div>
+      <SectionHeader
+        title="Account Details"
+        subtitle="Update your account details here"
+      >
+        <div className="mt-4 sm:grid sm:grid-cols-3 sm:items-start sm:gap-4  sm:border-gray-200 sm:pt-5">
+          <label
+            htmlFor="username"
+            className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+          >
+            Password
+          </label>
+          <div className="mt-1 sm:col-span-2 sm:mt-0">
+            <div className="flex max-w-lg rounded-md shadow-sm">
+              <input
+                type="text"
+                name="username"
+                id="username"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                autoComplete="username"
+                placeholder="Your New Password"
+                className="block w-full min-w-0 flex-1 rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              />
+              <Button
+                additionalStyling="ml-10"
+                text="Reset Password"
+                onClickHandler={() => {
+                  void supabaseClient.auth
+                    .updateUser({
+                      password,
+                    })
+                    .then(() => {
+                      toast.success("Succesfully updated user password");
+                    })
+                    .catch((err) => {
+                      toast.warning(err);
+                    });
+                }}
+              />
+            </div>
+          </div>
+        </div>
+      </SectionHeader>
+    </div>
+  );
 };
 
 export default Settings;
