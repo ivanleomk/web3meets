188f8e7a20ade47d35b352d86f8644d34a4ef497
Ivan Leo <ivanleo@Ivans-MacBook-Pro.local>
2023-04-26 11:21:59 +0800

    Fixed up abou us page

--- src/config/partners.ts
+++ src/config/partners.ts
@@ -2,25 +2,36 @@ export const PARTNERS = [
   {
     itemPath: "/Coinlive.png",
     label: "Coinlive",
+    href: "https://www.coinlive.com/",
   },
   {
     itemPath: "/Metacamp.png",
     label: "Metacamp",
+    href: "https://www.metacamp.so/",
   },
   {
     itemPath: "/Moledao.png",
     label: "Moledao",
+    href: "https://www.moledao.io/#/home",
   },
   {
     itemPath: "/Salad.png",
     label: "Salad Ventures",
+    href: "https://www.salad.ventures/",
   },
   {
     itemPath: "/Treehouse.png",
     label: "Treehouse",
+    href: "https://www.treehouse.finance/",
   },
   {
     itemPath: "/NUSFintechSociety.png",
     label: "NUS Fintech Society",
+    href: "https://fintechsociety.comp.nus.edu.sg/",
+  },
+  {
+    itemPath: "/talentweb3.png",
+    label: "Talent@Web3",
+    href: "https://talentweb3.co/",
   },
 ];
